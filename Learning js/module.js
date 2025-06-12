be5f028ac95fa22348ced13194e799df0c0656ae
Inthat Niramarn <inthat.n@ku.th>
2025-06-12 16:39:09 +0700

    Js

---
+++ Learning js/module.js
@@ -0,0 +1,9 @@
+export function add(a, b) {
+    return a + b;
+}
+
+export function greet() {
+    return "Hello world";
+}
+
+export const name = "John Doe"
---
+++ Learning js/script.js
@@ -0,0 +1,4 @@
+import {add, greet, name} from './module.js';
+
+console.log(add(5, 20));
+console.log(greet());
